32aa0fc5a35befba82b8595d9b045d3f091e11d2
jx@ovh2 <jx@ovh2>
2017-04-29 17:11:39 +0200

    readme and script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#install new server
+
+yum install -y epel-release
+yum update -y
+
+yum install -y vim-enhanced git nmap
+
+yum install -y ntp
+
+yum install  -y httpd mariadb-server mariadb php php-mysql php-mcrypt php-mbstring php-gd
+
+systemctl enable httpd
+systemctl start httpd
+systemctl enable mariadb
+systemctl start mariadb
+#mysql_secure_installation
+
+firewall-cmd --add-service=http --permanent
+firewall-cmd --add-service=https --permanent
+firewall-cmd --reload
+firewall-cmd --list-all --permanent
+nmap localhost
